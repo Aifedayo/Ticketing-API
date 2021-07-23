2450dac5bd20c9771c9f00ceb41d7546311ff142
akeemlag <akeemifedayolag@gmail.com>
2021-07-23 13:00:00 +0100

    generating aunique ticket id

--- ticketapi/ticketapi/tickets/models.py
+++ ticketapi/ticketapi/tickets/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
-# Create your models here.
+def generate_ticket_id():
+    # Generating a unique ticket id
+    return str(uuid.uuid4()).split("-")[-1]
