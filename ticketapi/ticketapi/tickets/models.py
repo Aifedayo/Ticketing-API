a5445729b4d625a52eb952bf0e9abfcd139e6086
akeemlag <akeemifedayolag@gmail.com>
2021-07-24 13:00:00 +0100

    creating a Ticket Model

--- ticketapi/ticketapi/tickets/models.py
+++ ticketapi/ticketapi/tickets/models.py
@@ -5,3 +5,11 @@ from django.contrib.auth.models import User
 def generate_ticket_id():
     # Generating a unique ticket id
     return str(uuid.uuid4()).split("-")[-1]
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    ticket_id = models.CharField(max_length=255, blank=True)
