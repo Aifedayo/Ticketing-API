649cd2cf5754a27d821a887f33cec648c1ff7c4d
akeemlag <akeemifedayolag@gmail.com>
2021-07-28 15:00:00 +0100

    creating the Category Model class

--- ticketapi/ticketapi/tickets/models.py
+++ ticketapi/ticketapi/tickets/models.py
@@ -32,3 +32,6 @@ class Ticket(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
