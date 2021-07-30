6907fe721d604b02bcd78a6fc44b686bb3ec87d2
akeemlag <akeemifedayolag@gmail.com>
2021-07-30 15:00:00 +0100

    HyperlinkedModelSerializer

--- ticketapi/ticketapi/tickets/serializers.py
+++ ticketapi/ticketapi/tickets/serializers.py
@@ -2,3 +2,8 @@ from rest_framework import serializers
 
 from django.contrib.auth.models import User
 from tickets.models import Ticket, Category
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
