6eb24b3f8097acd38a7ffddf2cc9487ae3018eaf
akeemlag <akeemifedayolag@gmail.com>
2021-08-02 15:00:00 +0100

    writing our views User...

--- ticketapi/ticketapi/tickets/views.py
+++ ticketapi/ticketapi/tickets/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 from rest_framework import routers, serializers, viewsets
 from ticketapi.serializers import UserSerializer, TicketSerializer, CategorySerializer
-# Create your views here.
+
+from django.contrib.auth.models import User
+from tickets.models import Ticket, Category
