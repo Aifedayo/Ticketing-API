6ecf781350e5d3fee93874082e9d6581e38f28ac
akeemlag <akeemifedayolag@gmail.com>
2021-08-06 15:00:00 +0100

    Routing our API

--- ticketapi/ticketapi/ticketapi/urls.py
+++ ticketapi/ticketapi/ticketapi/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth.models import User
+from tickets.models import Ticket, Category
+from rest_framework import routers
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
