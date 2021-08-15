93d6142480f2c7e2bd40a59523c1a18dfbd52c0c
akeemlag <akeemifedayolag@gmail.com>
2021-08-15 15:00:00 +0100

    rewriting UserSerializer

--- ticketapi/ticketapi/tickets/serializers.py
+++ ticketapi/ticketapi/tickets/serializers.py
@@ -14,6 +14,7 @@ class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     username = serializers.CharField(max_length=200)
     password = serializers.CharField(max_length=100, style={"input_type":"password"})
+    is_staff = serializers.BooleanField(default=False)
 
 
 class TicketSerializer(serializers.HyperlinkedModelSerializer):
